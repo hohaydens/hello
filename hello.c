46db3cffd121d21d3abbce26bc02962a9c8481c5
Hayden Holm <haydenholm085@gmail.com>
2026-08-27 13:02:12 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Hayden Holm
+ *         hholm3@huskers.unl.edu
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hayden Holm\n");
+
+  return 0;
+}
